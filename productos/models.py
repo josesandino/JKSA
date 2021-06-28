65c6606f5cf644baded01bd4667d4f2edd04ee15
joansamo01 <josesandinonio@gmail.com>
2021-06-28 13:21:18 -0600

    Class Categoria Producto | Footer

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    descripcion = models.CharField(max_length=64, null=False)
+
+    def __str__(self):
+        return self.descripcion
+
+class Producto(models.Model):
+    titulo       = models.CharField(max_length=260, null=False)
+    imagen       = models.ImageField(upload_to='imagenes/')
+    descripcion  = models.TextField()
+    precio       = models.DecimalField(decimal_places=2, max_digits=20, default=4.99)
+    categoria    = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Categoria, Producto
+
 # Create your views here.
 def home(request):
     return render(request, "base.html", {})
