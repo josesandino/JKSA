99b82306b0bee92a9216ee4eed5a9587593bb230
joansamo01 <josesandinonio@gmail.com>
2021-06-29 05:57:28 -0600

    Home Layout

--- productos/views.py
+++ productos/views.py
@@ -4,4 +4,12 @@ from .models import Categoria, Producto
 
 # Create your views here.
 def home(request):
-    return render(request, "base.html", {})
+    productos13 = Producto.objects.all()[:3]
+    productos410 = Producto.objects.all()[3:10]
+    categorias = Categoria.objects.all()
+    context = {
+        'primeros_productos': productos13,
+        'segundos_productos': productos410,
+        'lista_categorias': categorias,
+    }
+    return render(request, "home.html", context)
