312e3d3d7f0f975c57d4916642eaa11986e8ddf5
joansamo01 <josesandinonio@gmail.com>
2021-07-06 12:00:17 -0600

    Total opcion 1

--- carritos/views.py
+++ carritos/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 from .models import Carrito
 
 def carrito(request):
-    carrito_obj = Carrito.objects.new_or_get(request)
+    carrito_obj, nuevo_carrito = Carrito.objects.new_or_get(request)
+    productos = carrito_obj.productos.all()
+    total = 0
+    for x in productos:
+        total += x.precio
+    print(total)
+    carrito_obj.total = total
+    carrito_obj.save()
     return render(request, "carrito/carrito.html", {})
